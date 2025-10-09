1d6c2abac7ffb15e5f8914cfbb7c094436b2dcb7
elVengador <dev.jimynicanor@gmail.com>
2025-10-09 10:11:26 -0500

    feat(ev-icon): update sync to generate code and community

--- packages/icon-library/src/index.ts
+++ packages/icon-library/src/index.ts
@@ -2,6 +2,13 @@ import { defineMyIcon } from './defineIcon';
 
 defineMyIcon()
 
-export * from "./gen/brandIcons"
-export * from "./gen/regularIcons"
-export * from "./gen/solidIcons"
+export * from "./gen/base/brandIcons"
+export * from "./gen/base/regularIcons"
+export * from "./gen/base/solidIcons"
+
+export * from "./gen/community/brandIcons"
+export * from "./gen/community/regularIcons"
+export * from "./gen/community/solidIcons"
+export * from "./gen/community/gradientIcons"
+
+export * from "./gen/metadata"
