5ea906a3869d9cf4d07634cac65e19e1ebd589a9
elVengador <dev.jimynicanor@gmail.com>
2025-10-29 19:16:02 -0500

    chore(icons): return DomParser into to the class scope

--- packages/icons/src/defineIcon.ts
+++ packages/icons/src/defineIcon.ts
@@ -1,5 +1,3 @@
-const parser = new DOMParser();
-
 export const defineMyIcon = () => {
     // ℹ️ to run only on client
     if (typeof window === 'undefined') return
@@ -71,6 +69,7 @@ export const defineMyIcon = () => {
             if (!shadow) return;
 
             // ℹ️ DOMParser generally is more efficient than create template
+            const parser = new DOMParser();
             const doc = parser.parseFromString(svgRaw, 'image/svg+xml');
             const newSvgElement = doc.documentElement as unknown as SVGSVGElement | null;
 
