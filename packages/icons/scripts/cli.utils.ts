a55fc1b0010600366d80155447690b91f0ee967c
elVengador <dev.jimynicanor@gmail.com>
2025-11-04 16:09:27 -0500

    chore(icons): adding autoYes into sync question

--- packages/icons/scripts/cli.utils.ts
+++ packages/icons/scripts/cli.utils.ts
@@ -2,6 +2,11 @@ import readline from 'readline';
 
 export const askYN = async (message: string): Promise<boolean> => new Promise((resolve, reject) => {
     try {
+        const autoYes = process.env.CI === 'true' || process.argv.includes('--yes');
+        if (autoYes) {
+            return resolve(true)
+        }
+
         const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
