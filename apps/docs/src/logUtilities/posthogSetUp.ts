67a26dac0228f3f01d63874a7c43a58c11daa82e
elVengador <dev.jimynicanor@gmail.com>
2025-11-06 14:05:29 -0500

    chore: remove unused code

--- apps/docs/src/logUtilities/posthogSetUp.ts
+++ apps/docs/src/logUtilities/posthogSetUp.ts
@@ -1,6 +1,5 @@
 import posthog from 'posthog-js'
 
-console.log({ tt: import.meta.env, old: process?.env, node_env: process.env.NODE_ENV })
 if (process.env.NODE_ENV === 'production') {
     posthog.init(import.meta.env.PUBLIC_POSTHOG_KEY, {
         api_host: 'https://us.i.posthog.com',
